36623d96dde849f1a995015964fd5d71162665dd
Joseph Victor Zammit <jvzammit@gmail.com>
2022-08-26 12:42:26 +0200

    TIK-002 Adds SECRET_KEY to environment variables.

--- operqaas/settings.py
+++ operqaas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m%=nn2sgzd=vvxv4$484-5#=ljkoblj$(fxk#to%_)(qqq#vr4"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
